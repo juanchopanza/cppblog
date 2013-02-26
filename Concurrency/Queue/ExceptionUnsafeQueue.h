8911838374feac6a4573c66eb34b2f80d7a363b3
Juan Palacios <juan.palacios.puyana@gmail.com>
2013-02-26 15:29:15 +0100

    Files for future blog material.

---
+++ Concurrency/Queue/ExceptionUnsafeQueue.h
@@ -0,0 +1,36 @@
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+template <typename T>
+class ExceptionUnsafeQueue.h
+{
+ public:
+
+  T pop() 
+  {
+    std::unique_lock<std::mutex> mlock(mutex_);
+    while (queue_.empty())
+    {
+      cond_.wait(mlock);
+    }
+    auto val = queue_.front();
+    queue_.pop();
+    return val;
+  }
+
+  void push(const T& item)
+  {
+    std::lock_guard<std::mutex> mlock(mutex_);
+    queue_.push(item);
+    cond_.notify_one();
+  }
+
+ private:
+  std::queue<T> queue_;
+  std::mutex mutex_;
+  std::condition_variable cond_;
+};
+
+
---
+++ Concurrency/Queue/producer_consumer1.cpp
@@ -0,0 +1,40 @@
+#include "Queue.h"
+#include <iostream>
+
+void produce(Queue<int>& q) {
+  for (int i = 0; i< 10000; ++i) {
+    std::cout << "Pushing " << i << "\n";
+    q.push(i);
+  }
+}
+
+void consume(Queue<int>& q, unsigned int id) {
+  for (int i = 0; i< 10000; ++i) {
+    auto item = q.pop();
+    std::cout << "Consumer " << id << " popped " << item << "\n";
+  }
+}
+
+#include <thread>
+int main()
+{
+  Queue<int> q;
+
+  using namespace std::placeholders;
+
+  // producer thread
+  std::thread prod1(std::bind(produce, std::ref(q)));
+
+  // consumer threads
+  std::thread consumer1(std::bind(&consume, std::ref(q), 1));
+  std::thread consumer2(std::bind(&consume, std::ref(q), 2));
+  std::thread consumer3(std::bind(&consume, std::ref(q), 3));
+  std::thread consumer4(std::bind(&consume, std::ref(q), 4));
+
+  prod1.join();
+  consumer1.join();
+  consumer2.join();
+  consumer3.join();
+  consumer4.join();
+
+}
