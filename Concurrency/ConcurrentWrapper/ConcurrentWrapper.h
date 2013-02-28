deadd1137efb67bbfbfab70374ebabfa92e02cf6
Juan Palacios <juan.palacios.puyana@gmail.com>
2013-02-28 20:34:27 +0100

    Update ConcurrentWrapper.h
    Add some comments with links to the H. Sutter talk and some code samples.

--- Concurrency/ConcurrentWrapper/ConcurrentWrapper.h
+++ Concurrency/ConcurrentWrapper/ConcurrentWrapper.h
@@ -1,3 +1,33 @@
+///
+/// A concurrent wrapper for serializing asynchronous access to a shared resource.
+/// Taken from this excellent presentation on C++ concurrency by Herb Sutter:
+/// http://channel9.msdn.com/Shows/Going+Deep/C-and-Beyond-2012-Herb-Sutter-Concurrency-and-Parallelism
+///
+/// Wraps a shared resource such that callers can request operations on the resource asynchronously.
+/// Each request is treated as an atomic transaction, and the caller side is not blocked.
+///
+/// Internally uses concurrent queue developed for the concurrent queue blog
+/// http://juanchopanzacpp.wordpress.com/2013/02/26/concurrent-queue-c11/
+///
+/// Example useage:
+///
+/// @code
+/// ConcurrentWrapper<std::string> cs;
+///
+/// //Thread 1: just print the string
+/// cs([](const std::string& s){ std::cout << s;});
+/// 
+/// // Thread 2:
+/// void foo(std::string& s);
+/// void bar(std::string& s);
+/// // run bar, then foo on the shared resource
+/// cs([] (std::string& s) { bar(s); foo(s);});
+///
+/// // Thread 3: make string uppercase
+/// cs([](std::string& s){ for (auto& c : s) std::toupper(c);)} );
+///
+///
+
 #include <functional>
 #include <thread>
 #include "Queue/Queue.h"
