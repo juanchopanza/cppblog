3ea4bf765b0b5be384ded283ffc562f5ffe8e627
Juan Palacios <juan.palacios.puyana@gmail.com>
2014-08-17 10:06:23 +0200

    Update EventType.h
    Add reference to 2-clause BSD license

--- Patterns/Observer/EventType.h
+++ Patterns/Observer/EventType.h
@@ -1,3 +1,9 @@
+//
+// Copyright (c) 2013 Juan Palacios juan.palacios.puyana@gmail.com
+// Subject to the BSD 2-Clause License
+// - see < http://opensource.org/licenses/BSD-2-Clause>
+//
+
 #ifndef EVENTTYPE_H_
 #define EVENTTYPE_H_
 
